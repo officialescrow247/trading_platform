a53173a0152f673653eb52724fbf7f3175b0f1f7
Cletus Kingdom <cletuskingdom@gmail.com>
2024-07-29 15:53:16 +0100

    update

--- resources/views/new_design_2/index.blade.php
+++ resources/views/new_design_2/index.blade.php
@@ -1,37 +1,204 @@
 @extends('new_design_2.layouts.app')
 @section('content')
-    <section class="welcome_d">
-        <div class="container" id="harness_the_power">
-            <h1 class="_dess kanit_thin_font">Harness the power</h1>
-            <p class="text-light _dessw kanit_thin_font">of unreal spreads</p>
-            <p class="text-light _dess_w kanit_thin_font">
-                Get low-cost fixed spreads when you trade <br> with Trade Nation. 
-                That means when the <br> markets move, your spreads won't.
-            </p>
-            <img src="{{ asset('adv.avif') }}" class="adv">
-        </div>
-    </section>
+    <div style="background-color: #050F19;">
+        <section class="welcome_d">
+            <div class="container" id="harness_the_power">
+                <h1 class="_dess kanit_thin_font">Harness the power</h1>
+                <p class="text-light _dessw kanit_thin_font">of unreal spreads</p>
+                <p class="text-light _dess_w kanit_thin_font">
+                    Get low-cost fixed spreads when you trade <br> with Trade Nation. 
+                    That means when the <br> markets move, your spreads won't.
+                </p>
+                <img src="{{ asset('adv.avif') }}" class="adv">
+            </div>
+        </section>
 
-    <section class="so_ text-center">
-        <div class="collapse collapse-horizontal pb-4" id="joi_s">
-            <video id="joi_s_video" src="https://tnfev2.cdn.prismic.io/tnfev2/Zmc0i5m069VX1m_j_TN-HomepageVideoAV-16x9-V9.mp4" preload="auto" controls style="width: 100%; height: 100%;" playsinline webkit-playsinline x5-playsinline></video>
-            <hr>
-        </div>
+        <section class="so_ text-center">
+            <div class="collapse collapse-horizontal pb-4" id="joi_s">
+                <video id="joi_s_video" src="https://tnfev2.cdn.prismic.io/tnfev2/Zmc0i5m069VX1m_j_TN-HomepageVideoAV-16x9-V9.mp4" preload="auto" controls style="width: 100%; height: 100%;" playsinline webkit-playsinline x5-playsinline></video>
+                <hr>
+            </div>
 
-        <div class="ds_ww" data-bs-toggle="collapse" data-bs-target="#joi_s" aria-expanded="false" aria-controls="joi_s">
-            <p>
-                <b>Watch: Powering markets and smashing goals with Aston Villa Football Club</b>
-            </p>
-        </div>
-    </section>
+            <div class="ds_ww" data-bs-toggle="collapse" data-bs-target="#joi_s" aria-expanded="false" aria-controls="joi_s">
+                <p>
+                    <b>Watch: Powering markets and smashing goals with Aston Villa Football Club</b>
+                </p>
+            </div>
+        </section>
 
-    <section>
-        <div class="container py-5">
-            <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum qui modi delectus et magni odio sed minima sequi rem accusamus, nesciunt dolore illo, dolores nostrum sunt ratione, nobis porro perferendis?
-            </p>
-        </div>
-    </section>
+        <section>
+            <div class="container py-5 text-center text-light dsq_">
+                <h3 class="pt-5">Meet the <b class="kanit_thin_font">game changers</b></h3>
+
+                <div class="pt-3">
+                    <p>
+                        Other brokers offer variable spreads but with Trade Nation's low-cost fixed <br>
+                        spreads, when the markets move, your costs don't.
+                    </p>
+
+                    <p>
+                        It's no wonder we're the pioneers of trust, value and transparency in trading. <br>
+                    </p>
+                    
+                    <p>
+                        So, no matter how experienced you are at trading, you can thrive at Trade Nation.
+                    </p>
+
+                    <p class="pt-3" style="font-size: 25px; color: #fc5000;"><b>And that's what makes us unreal.</b></p>
+
+                    <div style="display: flex;" class="justify-content-center pt-5"><div class="hrr"></div></div>
+                </div>
+            </div>
+        </section>
+
+        <section class="container">
+            <div class="py-5 text-light">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="ok_">
+                            <h3 class="kanit_thin_font">
+                                <b>Play the long game.</b>
+                            </h3>
+                            <p class="oks_ inter">
+                                And the short
+                            </p>
+                            
+                            <p class="inter">
+                                We have unparalleled spread trading and CFD trading <br> expertise and 1000's of markets are waiting for you - so we <br> help you trade your way.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 c_d">
+                        <div class="text-light">
+                            <div class="row">
+                                <div class="col-md-6 mb-4">
+                                    <div>
+                                        <img src="https://images.prismic.io/tnfev2/ZkX7biol0Zci9M_t_home-icon-1.png?auto=compress%2Cformat&fit=max&w=1920" alt="" class="img-fluid w-50">
+    
+                                        <div class="pt-4 inter">
+                                            <a href="#!" style="font-size: 25px;" class="text-light"><b>Forex</b></a>
+                                            <p>The buying and selling of global currencies</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                                <div class="col-md-6 mb-4">
+                                    <div>
+                                        <img src="https://images.prismic.io/tnfev2/ZkX7biol0Zci9M_u_home-icon-2.png?auto=compress%2Cformat&fit=max&w=1920" alt="" class="img-fluid w-50">
+    
+                                        <div class="pt-4 inter">
+                                            <a href="#!" style="font-size: 25px;" class="text-light"><b>Commodities</b></a>
+                                            <p>The exchange of raw goods that fuel the globe</p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-6 mb-4">
+                                    <div>
+                                        <img src="https://images.prismic.io/tnfev2/ZkX7VSol0Zci9M_o_home-icon-three.png?auto=compress%2Cformat&fit=max&w=1920" alt="" class="img-fluid w-50">
+    
+                                        <div class="pt-4 inter">
+                                            <a href="#!" style="font-size: 25px;" class="text-light"><b>Indices</b></a>
+                                            <p>Tracking the financial world's market titans</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                                <div class="col-md-6 mb-4">
+                                    <div>
+                                        <img src="https://images.prismic.io/tnfev2/ZkX7VSol0Zci9M_n_home-icon-4.png?auto=compress%2Cformat&fit=max&w=1920" alt="" class="img-fluid w-50">
+    
+                                        <div class="pt-4 inter">
+                                            <a href="#!" style="font-size: 25px;" class="text-light"><b>Shares</b></a>
+                                            <p>Bite-sized pieces of companies</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="okF_">
+                <div class="row">
+                    <div class="col-lg-6 col-md-12">
+                        <img src="ZkxUjSol0Zci9TtX_Phone-hand-2-.avif">
+                    </div>
+
+                    <div class="col-lg-6 col-md-12">
+                        <div>
+                            <div class="ok_ text-light pt-5">
+                                <h3 class="kanit_thin_font">
+                                    <b>Seamless trading</b> from <br>
+                                    the palm of your hand
+                                </h3>
+                                <p class="oks_ inter">
+                                    And the short
+                                </p>
+                                
+                                <p class="inter pb-3">
+                                    Smash your trading goals on desktop or mobile with powerful <br> tools like our daily report, chart features, and signal centre.
+                                </p>
+
+                                <p class="pb-1"><b>Trade now on:</b></p>
+                                <div class="d-none d-lg-block">
+                                    <div class="row">
+                                        <div class="col-lg-4 mb-3">
+                                            <a href="#!" class="d-flex justify-content-center text-light border rounded-pill py-1 px-3" style="text-decoration: none; align-items: end; display: none;">
+                                                <div><i style="font-size: 20px;" class="fa fa-desktop pe-2"></i></div>
+                                                <div class="inter" style="font-size: 16px;">Desktop</div>
+                                            </a>
+                                        </div>
+    
+                                        <div class="col-lg-4 mb-3">
+                                            <a href="#!" class="d-flex justify-content-center text-light border rounded-pill py-1 px-3" style="text-decoration: none; align-items: end; display: none;">
+                                                <div><i style="font-size: 20px;" class="fa fa-tablet pe-2"></i></div>
+                                                <div class="inter" style="font-size: 16px;">Google Play</div>
+                                            </a>
+                                        </div>
+    
+                                        <div class="col-lg-4 mb-3">
+                                            <a href="#!" class="d-flex justify-content-center text-light border rounded-pill py-1 px-3" style="text-decoration: none; align-items: end; display: none;">
+                                                <div><i style="font-size: 20px;" class="fa fa-tablet pe-2"></i></div>
+                                                <div class="pt-1 inter" style="font-size: 16px;">App Store</div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="d-lg-none">
+                                    <div class="d-flex justify-content-around">
+                                        <div class="mb-3">
+                                            <a href="#!" class="d-flex justify-content-center text-light border rounded-pill py-1 px-3" style="text-decoration: none; align-items: flex-start; display: none;">
+                                                <div><i style="font-size: 20px;" class="fa fa-desktop pe-2"></i></div>
+                                                <div class="inter" style="font-size: 14px;">Desktop</div>
+                                            </a>
+                                        </div>
+    
+                                        <div class="mb-3">
+                                            <a href="#!" class="d-flex justify-content-center text-light border rounded-pill py-1 px-3" style="text-decoration: none; align-items: flex-start; display: none;">
+                                                <div><i style="font-size: 20px;" class="fa fa-tablet pe-2"></i></div>
+                                                <div class="inter" style="font-size: 14px;">Google Play</div>
+                                            </a>
+                                        </div>
+    
+                                        <div class="mb-3">
+                                            <a href="#!" class="d-flex justify-content-center text-light border rounded-pill py-1 px-3" style="text-decoration: none; align-items: flex-start; display: none;">
+                                                <div><i style="font-size: 20px;" class="fa fa-tablet pe-2"></i></div>
+                                                <div class="inter" style="font-size: 14px;">App Store</div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
 
     <script>
         document.querySelector('.ds_ww').addEventListener('click', function() {
--- resources/views/new_design_2/layouts/app.blade.php
+++ resources/views/new_design_2/layouts/app.blade.php
@@ -19,105 +19,99 @@
         <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
     </head>
 
-    <body>
-        <header class="fixed-top bg_c">
-            <div class="d-flex container pt-3" style="justify-content: space-between; align-items: center;">
-                <p class="m-0 p-0 d_">
-                    Trading CFDs carries a high level of risk to your capital, and you should only trade with money you can afford to lose.
-                </p>
+    <body class="bg_c">
+        <div class="mb-2 py-3 border border-top-0 border-start-0 border-end-0" style="background-color: #050f19;">
+            <div class="d-flex container" style="justify-content: space-between; align-items: center;">
+                <div class="d_ d-flex">
+                    <p class="p-0 m-0">
+                        Trading CFDs carries a high level of risk to your capital, and you should only trade with money you can afford to lose.
+                    </p>
+                </div>
     
-                <p class="m-0 p-0">
+                <div>
                     <span class="d_2">Trading in</span>
                     <img src="{{ asset('imgs/en-sc.svg') }}" alt="" class="img-fluid" style="width: 17px;">
                     <span class="d_2">SC</span>
+                </div>
+            </div>
+        </div>
+
+        <nav class="navbar navbar-expand-lg navbar-light sticky-top" style="background-color: #050f19;">
+            <div class="container pb-3">
+                <a class="navbar-brand text-white" href="{{ url('/') }}">
+                    <img src="{{ asset('imgs/logo.avif') }}" alt="{{ env('APP_NAME') }} logo" style="width: 300px;">    
+                </a>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
+                    data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation" style="background-color: #fff;">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse pt-3" id="navbarNavDropdown">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item me-3">
+                            <a class="nav-link text-light" href="#">
+                                <i class="fa fa-magnifying-glass"></i>
+                            </a>
+                        </li>
+
+                        <li class="nav-item" style="border-left: 1px solid white;">
+                            <a class="nav-link active text-light" aria-current="page" href="#"></a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link active text-light" aria-current="page" href="#">Trading</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="#">Platform</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="#">Learn</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="#">Partners</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="#">About us</a>
+                        </li>
+
+                        <!-- <li class="nav-item dropdown">
+                            <a class="nav-link text-light dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Dropdown link
+                            </a>
+
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li> -->
+
+                        <li class="nav-item ms-3">
+                            <a class="nav-link rounded-pill sign_in_login_btn px-4 text-dark" href="#"><b>Sign up / Log in</b></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <!-- <div class="d-none d-flex container pt-3" style="justify-content: space-between; align-items: center;">
+            <div class="d_ d-flex">
+                <p class="p-0 m-0">
+                    Trading CFDs carries a high level of risk to your capital, and you should only trade with money you can afford to lose.
                 </p>
             </div>
-            
-            <hr>
 
-            <!-- <nav class="navbar navbar-expand-lg navbar-light container">
-				<div class="">
-					<a class="navbar-brand text-white" href="{{ url('/') }}">
-                        <img src="{{ asset('imgs/logo.avif') }}" alt="{{ env('APP_NAME') }} logo" style="width: 300px;">    
-                    </a>
-                    
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-
-                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div class="navbar-nav">
-                            <a class="text-light nav-link active" aria-current="page" href="#">Home</a>
-                            <a class="text-light nav-link" href="#">Features</a>
-                            <a class="text-light nav-link" href="#">Pricing</a>
-                        </div>
-                    </div>
-				</div>
-			</nav> -->
-
-            <nav class="navbar navbar-expand-lg navbar-light">
-                <div class="container pb-3">
-                    <a class="navbar-brand text-white" href="{{ url('/') }}">
-                        <img src="{{ asset('imgs/logo.avif') }}" alt="{{ env('APP_NAME') }} logo" style="width: 300px;">    
-                    </a>
-
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
-                        data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation" style="background-color: #fff;">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-
-                    <div class="collapse navbar-collapse pt-3" id="navbarNavDropdown">
-                        <ul class="navbar-nav ms-auto">
-                            <li class="nav-item me-3">
-                                <a class="nav-link text-light" href="#">
-                                    <i class="fa fa-magnifying-glass"></i>
-                                </a>
-                            </li>
-
-                            <li class="nav-item" style="border-left: 1px solid white;">
-                                <a class="nav-link active text-light" aria-current="page" href="#"></a>
-                            </li>
-
-                            <li class="nav-item">
-                                <a class="nav-link active text-light" aria-current="page" href="#">Trading</a>
-                            </li>
-
-                            <li class="nav-item">
-                                <a class="nav-link text-light" href="#">Platform</a>
-                            </li>
-
-                            <li class="nav-item">
-                                <a class="nav-link text-light" href="#">Learn</a>
-                            </li>
-
-                            <li class="nav-item">
-                                <a class="nav-link text-light" href="#">Partners</a>
-                            </li>
-
-                            <li class="nav-item">
-                                <a class="nav-link text-light" href="#">About us</a>
-                            </li>
-
-                            <!-- <li class="nav-item dropdown">
-                                <a class="nav-link text-light dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Dropdown link
-                                </a>
-
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="#">Action</a></li>
-                                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
-                            </li> -->
-
-                            <li class="nav-item ms-3">
-                                <a class="nav-link rounded-pill sign_in_login_btn px-4 text-dark" href="#"><b>Sign up / Log in</b></a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </header>
+            <div>
+                <span class="d_2">Trading in</span>
+                <img src="{{ asset('imgs/en-sc.svg') }}" alt="" class="img-fluid" style="width: 17px;">
+                <span class="d_2">SC</span>
+            </div>
+        </div> -->
         
         <main>@yield('content')</main>
         
