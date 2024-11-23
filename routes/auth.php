aaa667751d5f7ff5f375902a462b21192d591109
Cletus Kingdom <cletuskingdom@gmail.com>
2024-11-23 09:18:46 +0100

    update

--- routes/auth.php
+++ routes/auth.php
@@ -54,49 +54,52 @@ Route::middleware('guest')->group(function () {
             $code = rand(100000, 999999);
             $user = Auth::user();
     
-            if($user->security_on_or_off == 0){
-                // log him in
-                $user->security_verified = true;
-                $user->save();
-    
-                $user = User::find($user->id);
-                Auth::login($user);
-                if ($user->hasRole(['admin','superadmin'])) {
-                    return redirect()->route('dashboard');
-                }else{
-                    return redirect()->route('dashboard');
-                }
-            }
-    
-            // Store the PIN in session
-            SecurityPin::create([
-                "code" => $code,                
-                "user_id" => $user->id,              
-                "status" => 0,              
-            ]);
-    
-            // Set security_verified to false in the database
-            $user->security_verified = false;
-            $user->save();
-    
-            // Send main to the user
-            $data = [
-                'admin_email' => env('MAIL_FROM_ADDRESS'),
-                'site_name' => env('APP_NAME'),
-                'user_name' => Auth::user()->first_name,
-                'user_email' => Auth::user()->email,
-                'msg' => "On " . Carbon::now() . " you attempted to login to your dashboard. 
-                If this was you, please use the code below to proceed with the login: " . $code
-            ];
+            if($user){
 
-            Mail::send('mails.email_template2', $data, function ($message) use ($data) {
-                $message->from($data['admin_email'], $data['site_name']);
-                $message->to($data['user_email'], $data['user_name']);
-                $message->subject('CONFIRM YOUR SECURITY CODE AND LOGIN');
-            });
+                if($user->security_on_or_off == 0){
+                    // log him in
+                    $user->security_verified = true;
+                    $user->save();
+        
+                    $user = User::find($user->id);
+                    Auth::login($user);
+                    if ($user->hasRole(['admin','superadmin'])) {
+                        return redirect()->route('dashboard');
+                    }else{
+                        return redirect()->route('dashboard');
+                    }
+                }
+        
+                // Store the PIN in session
+                SecurityPin::create([
+                    "code" => $code,                
+                    "user_id" => $user->id,              
+                    "status" => 0,              
+                ]);
+        
+                // Set security_verified to false in the database
+                $user->security_verified = false;
+                $user->save();
+        
+                // Send main to the user
+                $data = [
+                    'admin_email' => env('MAIL_FROM_ADDRESS'),
+                    'site_name' => env('APP_NAME'),
+                    'user_name' => Auth::user()->first_name,
+                    'user_email' => Auth::user()->email,
+                    'msg' => "On " . Carbon::now() . " you attempted to login to your dashboard. 
+                    If this was you, please use the code below to proceed with the login: " . $code
+                ];
     
-            // Redirect the user to the PIN verification page
-            return redirect()->route('login_verify_pin');
+                Mail::send('mails.email_template2', $data, function ($message) use ($data) {
+                    $message->from($data['admin_email'], $data['site_name']);
+                    $message->to($data['user_email'], $data['user_name']);
+                    $message->subject('CONFIRM YOUR SECURITY CODE AND LOGIN');
+                });
+        
+                // Redirect the user to the PIN verification page
+                return redirect()->route('login_verify_pin');
+            }
         }
     
         // If login fails
