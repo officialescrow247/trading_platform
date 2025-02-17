804da72f7ec473eff74296cc2cd135d9c51f4819
Cletus Kingdom <cletuskingdom@gmail.com>
2025-02-17 21:35:26 +0100

    update on the confirmation mail

--- resources/views/mails/email_template2.blade.php
+++ resources/views/mails/email_template2.blade.php
@@ -38,7 +38,7 @@
                         We noticed an attempt to access your account on {{ $site_name }} at 
                         {{ $datee }}, If this was you, please enter the following code 
                         to complete your session: <b>{{ $code }}</b>. If you did not attempt this, 
-                        please contact support immediately." 
+                        please contact support immediately.
                     </p>
 
                     <p style="padding-top: 40px; font-weight: 600;">
