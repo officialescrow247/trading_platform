25d37d872d2b0ca53bee2d9337e751b683ee8bfc
Cletus Kingdom <cletuskingdom@gmail.com>
2025-06-16 10:56:37 +0100

    updated on google

--- resources/views/new_design_2/layouts/app.blade.php
+++ resources/views/new_design_2/layouts/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>{{ env('APP_NAME') }}</title>
+        <meta name="google-site-verification" content="C5ZqEsVUHxqH2BI9jTnSDAeIKjyspb--jny_5CzHGCg" />
 
         <!-- Bootstrap css -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
